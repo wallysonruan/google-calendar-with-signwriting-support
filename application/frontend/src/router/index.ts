3e88df9c5444911dca5c00ce5f9c5b92298ac634
wallyson ruan <wallysonruan@gmail.com>
2023-10-03 16:42:16 -0300

    fix: Fixed path importation

--- application/frontend/src/router/index.ts
+++ application/frontend/src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import Activities from '@/components/Activities.vue'
+import Activities from '@/components/ActivitiesList.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
