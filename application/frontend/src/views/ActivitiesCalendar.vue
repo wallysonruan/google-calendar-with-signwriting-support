8fda82977eaf48bafcd4b74301c5dee959987d27
wallyson ruan <wallysonruan@gmail.com>
2023-10-04 20:55:26 -0300

    fix: lint boring

--- application/frontend/src/views/ActivitiesCalendar.vue
+++ application/frontend/src/views/ActivitiesCalendar.vue
@@ -6,7 +6,7 @@ const days = [2, 3, 4, 5, 6, 7, 8, 9]
 
 <template>
   <div class="calendar">
-    <CalendarDay class="calendar-day" v-for="day in days" :number="day" />
+    <CalendarDay class="calendar-day" v-for="day in days" :number="day" v-bind:key="day"/>
   </div>
 </template>
 
