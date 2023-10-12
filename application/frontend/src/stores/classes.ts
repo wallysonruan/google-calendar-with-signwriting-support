0b777decf8e6be448f817d4966cebbd25c4017ee
wallyson ruan <wallysonruan@gmail.com>
2023-10-12 02:40:55 -0300

    wip: added new dates to check how its working

--- application/frontend/src/stores/classes.ts
+++ application/frontend/src/stores/classes.ts
@@ -14,7 +14,38 @@ export type classItem = {
 
 const classesData: classItem[] = [
   {
-    date: new Date('2023-08-10'),
+    date: new Date('2023-01-10'),
+    class_title: {
+      libras: 'M24x19S16d20n11xn17S26a0610xn19S20e00n24xn3S2031an10x4 ',
+      pt: 'Aula de Português',
+      eng: ''
+    },
+    course_title: {
+      libras: 'M24x19S16d20n11xn17S26a0610xn19S20e00n24xn3S2031an10x4 ',
+      pt: 'Curso de Tradução',
+      eng: ''
+    },
+    activities: [
+      {
+        title: {
+          libras:
+            'AS16d3eS30004S22f04M540x541S30004482x482S16d3e488x523S22f04515x526 AS16d3eS30004S22f04M540x541S30004482x482S16d3e488x523S22f04515x526',
+          pt: 'Atividade 1',
+          eng: ''
+        }
+      },
+      {
+        title: {
+          libras:
+            'AS16d3eS30004S22f04M540x541S30004482x482S16d3e488x523S22f04515x526 AS16d3eS30004S22f04M540x541S30004482x482S16d3e488x523S22f04515x526',
+          pt: 'Atividade 2',
+          eng: ''
+        }
+      }
+    ]
+  },
+  {
+    date: new Date('2023-01-09'),
     class_title: {
       libras: 'M24x19S16d20n11xn17S26a0610xn19S20e00n24xn3S2031an10x4 ',
       pt: 'Aula de Português',
@@ -76,7 +107,7 @@ const classesData: classItem[] = [
     ]
   },
   {
-    date: new Date('2021-08-10'),
+    date: new Date('2021-04-10'),
     class_title: {
       libras: 'M24x19S16d20n11xn17S26a0610xn19S20e00n24xn3S2031an10x4 ',
       pt: 'Aula de Português',
@@ -115,7 +146,7 @@ const classesData: classItem[] = [
     ]
   },
   {
-    date: new Date('2021-08-02'),
+    date: new Date('2021-06-02'),
     class_title: {
       libras: 'M24x19S16d20n11xn17S26a0610xn19S20e00n24xn3S2031an10x4 ',
       pt: 'Aula de Português',
