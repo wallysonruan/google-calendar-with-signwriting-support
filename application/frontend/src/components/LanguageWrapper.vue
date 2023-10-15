44027862bb17d764c516c6f0d212076684f9f0b8
wallysonruan <wallysonruan@gmail.com>
2023-10-15 19:02:20 -0300

    feat: added Libras translation to create-class view

---
+++ application/frontend/src/components/LanguageWrapper.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import SignWriting from './SignWriting.vue'
+import { useLanguageStore } from '@/stores/language'
+const props = defineProps<{
+  sign: string
+  width?: number
+  height?: number
+  portuguese: string
+}>()
+</script>
+<template>
+  <div>
+    <p v-if="useLanguageStore().portuguese">
+      {{ props.portuguese }}
+    </p>
+    <SignWriting
+      v-if="useLanguageStore().signwriting"
+      :sign="props.sign"
+      :width="props.width"
+      :height="props.height"
+      :display="useLanguageStore().signwriting"
+    />
+  </div>
+</template>
