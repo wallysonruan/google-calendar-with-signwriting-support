5261e6000753e8a045c4b3abd652887211d36952
wallyson ruan <wallysonruan@gmail.com>
2023-10-16 17:16:55 -0300

    feat: centralized btns and fields, also changed layout width on desktop

--- application/frontend/src/views/CreateClass.vue
+++ application/frontend/src/views/CreateClass.vue
@@ -3,8 +3,10 @@ import type { classItem } from '@/stores/classes'
 import { ref } from 'vue'
 import { useClassesStore } from '@/stores/classes'
 import LanguageWrapper from '@/components/LanguageWrapper.vue'
+import { useLanguageStore } from '@/stores/language'
 
 const classStore = useClassesStore()
+const languageStore = useLanguageStore()
 
 const classItemModel = ref<classItem>({
   date: new Date(),
@@ -51,85 +53,87 @@ const rules = {
 }
 </script>
 <template>
-  <v-form @submit.prevent="createClass(classItemModel)">
-    <h2>
-      <LanguageWrapper
-        :sign="'M24x19S16d20n11xn17S26a0610xn19S20e00n24xn3S2031an10x4 AS11520S26606M524x515S11520475x485S26606494x486'"
-        :portuguese="'Nome do Curso'"
-        :height="30"
-        :width="30"
-      />
-    </h2>
-    <!-- <label for="course_pt">Português</label> -->
-    <v-text-field
-      v-model="classItemModel.course_title.pt"
-      class="input"
-      id="course_pt"
-      label="Português"
-      :rules="[rules.required.pt]"
-    ></v-text-field>
-    <!-- <label for="course_libras">Libras</label> -->
-    <v-text-field
-      v-model="classItemModel.course_title.libras"
-      class="input"
-      id="course_libras"
-      label="Libras"
-      hint="Insira aqui a sequência FSW do sinal desejado."
-      :rules="[rules.required.libras]"
-    ></v-text-field>
+  <v-form class="form" @submit.prevent="createClass(classItemModel)">
+    <div class="inputs">
+      <h2>
+        <LanguageWrapper
+          :sign="'M24x19S16d20n11xn17S26a0610xn19S20e00n24xn3S2031an10x4 AS11520S26606M524x515S11520475x485S26606494x486'"
+          :portuguese="'Nome do Curso'"
+          :height="30"
+          :width="30"
+        />
+      </h2>
+      <!-- <label for="course_pt">Português</label> -->
+      <v-text-field
+        v-model="classItemModel.course_title.pt"
+        class="input"
+        id="course_pt"
+        label="Português"
+        :rules="[rules.required.pt]"
+      ></v-text-field>
+      <!-- <label for="course_libras">Libras</label> -->
+      <v-text-field
+        v-model="classItemModel.course_title.libras"
+        class="input"
+        id="course_libras"
+        label="Libras"
+        hint="Insira aqui a sequência FSW do sinal desejado."
+        :rules="[rules.required.libras]"
+      ></v-text-field>
 
-    <h2>
-      <LanguageWrapper
-        :sign="'M524x516S15a39477x485S15a31490x493S20600502x485 AS11520S26606M524x515S11520475x485S26606494x486'"
-        :portuguese="'Nome da Aula'"
-        :height="30"
-        :width="30"
-      />
-    </h2>
-    <!-- <label for="class_pt">Português</label> -->
-    <v-text-field
-      v-model="classItemModel.class_title.pt"
-      class="input"
-      id="class_pt"
-      label="Português"
-      :rules="[rules.required.pt]"
-    ></v-text-field>
-    <!-- <label for="class_libras">Libras</label> -->
-    <v-text-field
-      v-model="classItemModel.class_title.libras"
-      class="input"
-      id="class_libras"
-      label="Libras"
-      hint="Insira aqui a sequência FSW do sinal desejado."
-      :rules="[rules.required.libras]"
-    ></v-text-field>
+      <h2>
+        <LanguageWrapper
+          :sign="'M524x516S15a39477x485S15a31490x493S20600502x485 AS11520S26606M524x515S11520475x485S26606494x486'"
+          :portuguese="'Nome da Aula'"
+          :height="30"
+          :width="30"
+        />
+      </h2>
+      <!-- <label for="class_pt">Português</label> -->
+      <v-text-field
+        v-model="classItemModel.class_title.pt"
+        class="input"
+        id="class_pt"
+        label="Português"
+        :rules="[rules.required.pt]"
+      ></v-text-field>
+      <!-- <label for="class_libras">Libras</label> -->
+      <v-text-field
+        v-model="classItemModel.class_title.libras"
+        class="input"
+        id="class_libras"
+        label="Libras"
+        hint="Insira aqui a sequência FSW do sinal desejado."
+        :rules="[rules.required.libras]"
+      ></v-text-field>
 
-    <h2>
-      <LanguageWrapper
-        :sign="'M530x526S15a37471x474S1f751474x486S14c51490x500S27e0c507x489 AS11520S26606M524x515S11520475x485S26606494x486'"
-        :portuguese="'Nome da Atividade'"
-        :height="30"
-        :width="30"
-      />
-    </h2>
-    <!-- <label for="activity_pt">Português</label> -->
-    <v-text-field
-      v-model="classItemModel.activities[0].title.pt"
-      class="input"
-      id="activity_pt"
-      label="Português"
-      :rules="[rules.required.pt]"
-    ></v-text-field>
-    <!-- <label for="activity_libras">Libras</label> -->
-    <v-text-field
-      v-model="classItemModel.activities[0].title.libras"
-      class="input"
-      id="activity_libras"
-      label="Libras"
-      hint="Insira aqui a sequência FSW do sinal desejado."
-      :rules="[rules.required.libras]"
-    ></v-text-field>
-    <v-btn height="fit-content" :loading="loading" type="submit">
+      <h2>
+        <LanguageWrapper
+          :sign="'M530x526S15a37471x474S1f751474x486S14c51490x500S27e0c507x489 AS11520S26606M524x515S11520475x485S26606494x486'"
+          :portuguese="'Nome da Atividade'"
+          :height="30"
+          :width="30"
+        />
+      </h2>
+      <!-- <label for="activity_pt">Português</label> -->
+      <v-text-field
+        v-model="classItemModel.activities[0].title.pt"
+        class="input"
+        id="activity_pt"
+        label="Português"
+        :rules="[rules.required.pt]"
+      ></v-text-field>
+      <!-- <label for="activity_libras">Libras</label> -->
+      <v-text-field
+        v-model="classItemModel.activities[0].title.libras"
+        class="input"
+        id="activity_libras"
+        label="Libras"
+        hint="Insira aqui a sequência FSW do sinal desejado."
+        :rules="[rules.required.libras]"
+      ></v-text-field>
+    </div>
+    <v-btn class="create-btn" :loading="loading" type="submit" :sw="languageStore.signwriting">
       <LanguageWrapper
         :sign="'M531x540S16d48470x494S1773e476x520S22b00515x486S15730471x460S21000492x498'"
         :portuguese="'Criar'"
@@ -140,10 +144,33 @@ const rules = {
 </template>
 <style scoped lang="scss">
 form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   width: 100%;
 
-  .input {
+  .inputs {
     width: 100%;
+  }
+
+  .create-btn {
+    width: max-content;
+
+    &[sw='true'] {
+      height: fit-content;
+    }
+  }
+}
+
+@media only screen and (min-width: 600px) {
+  form {
+    margin-left: auto;
+    margin-right: auto;
+    width: 60%;
+
+    .inputs {
+      width: 100%;
+    }
   }
 }
 </style>
