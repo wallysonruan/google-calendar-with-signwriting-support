4315c2a93d9f843d1ee7899634c5ace23b04cd05
wallyson ruan <wallysonruan@gmail.com>
2023-10-16 17:01:40 -0300

    fix: CreateClass was not closing the drawer properly

--- application/frontend/src/components/NavBarMobile.vue
+++ application/frontend/src/components/NavBarMobile.vue
@@ -9,7 +9,7 @@ const drawer = ref<boolean>(false)
   <v-navigation-drawer location="right" class="drawer" v-model="drawer">
     <template v-slot:append>
       <v-list class="list">
-        <v-list-item link @click.stop="drawer = !drawer">
+        <v-list-item link>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="16"
@@ -25,7 +25,7 @@ const drawer = ref<boolean>(false)
           &nbsp;
           <RouterLink to="/" @click.stop="drawer = !drawer"> Página Inicial </RouterLink>
         </v-list-item>
-        <v-list-item link @click.stop="drawer = !drawer">
+        <v-list-item link>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="16"
@@ -40,7 +40,7 @@ const drawer = ref<boolean>(false)
             />
           </svg>
           &nbsp;
-          <RouterLink to="create-class"> Criar Aula </RouterLink>
+          <RouterLink to="create-class" @click.stop="drawer = !drawer"> Criar Aula </RouterLink>
         </v-list-item>
         <v-list-item link @click="classesStore.deleteAllClasses">
           <svg
