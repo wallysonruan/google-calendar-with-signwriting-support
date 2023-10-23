11240b0ce44aadf6ae13ed0478506432914943c2
wallyson ruan <wallysonruan@gmail.com>
2023-10-23 12:29:12 -0300

    refactor: vite now understands all fsw-* tags as custom

--- application/frontend/vite.config.ts
+++ application/frontend/vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     vue({
       template: {
         compilerOptions: {
-          isCustomElement: (tag) => tag.includes('fsw-sign')
+          isCustomElement: (tag) => tag.startsWith('fsw-')
         }
       }
     }),
