6676d2d4cdf9f56ebbae1b38d538067e5477ceef
wallyson ruan <wallysonruan@gmail.com>
2023-10-24 19:07:59 -0300

    refactor: all signs, except the last, will get a margin-bottom

--- application/frontend/src/components/SignWriting.vue
+++ application/frontend/src/components/SignWriting.vue
@@ -8,17 +8,21 @@ const props = defineProps<{
 </script>
 
 <template>
-  <div class="sign" v-show="display">
-    <div v-for="sign in props.fsw.split(' ')" >
-      <fsw-sign :sign="sign" v-if="!isType(sign, 'punctuation')"/>
-      <fsw-symbol :symbol="sign" v-if="isType(sign, 'punctuation')"/>
+  <div v-show="display">
+    <div v-for="sign in props.fsw.split(' ')" class="signs">
+      <fsw-sign :sign="sign" v-if="!isType(sign, 'punctuation')" />
+      <fsw-symbol :symbol="sign" v-if="isType(sign, 'punctuation')" />
     </div>
   </div>
 </template>
 <style scoped lang="scss">
-.sign {
+.signs {
   display: flex;
   flex-direction: column;
   align-items: center;
+
+  &:not(:last-child) {
+    margin-bottom: 0.3rem;
+  }
 }
 </style>
