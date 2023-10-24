9f6e9e43e5e242d1d8c283e68dea4f7a8b97b2e6
wallyson ruan <wallysonruan@gmail.com>
2023-10-24 20:20:55 -0300

    refactor: added sw to mobile navbar

--- application/frontend/src/components/NavBarMobile.vue
+++ application/frontend/src/components/NavBarMobile.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import LanguageSwitcher from './LanguageSwitcher.vue'
+import LanguageWrapper from './LanguageWrapper.vue'
 import { useClassesStore } from '@/stores/classes'
 import { useAlertStore } from '@/stores/alert'
 const classesStore = useClassesStore()
@@ -11,52 +12,71 @@ const drawer = ref<boolean>(false)
     <template v-slot:append>
       <v-list class="list">
         <v-list-item link @click.stop="drawer = !drawer">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            fill="currentColor"
-            class="bi bi-house-door"
-            viewBox="0 0 16 16"
-          >
-            <path
-              d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146ZM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4H2.5Z"
-            />
-          </svg>
-          &nbsp;
-          <RouterLink to="/"> Página Inicial </RouterLink>
+          <div class="list-item">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-house-door"
+              viewBox="0 0 16 16"
+            >
+              <path
+                d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146ZM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4H2.5Z"
+              />
+            </svg>
+            <RouterLink to="/">
+              <LanguageWrapper
+                class="language-wrapper"
+                sign="M533x515S1dc20509x485S1dc28467x485 M520x525S1f502492x476S23604481x508"
+                portuguese="Página Inicial"
+            /></RouterLink>
+          </div>
         </v-list-item>
         <v-list-item link @click.stop="drawer = !drawer">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            fill="currentColor"
-            class="bi bi-plus-lg"
-            viewBox="0 0 16 16"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z"
-            />
-          </svg>
-          &nbsp;
-          <RouterLink to="create-class"> Criar Aula </RouterLink>
+          <div class="list-item">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-plus-lg"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z"
+              />
+            </svg>
+            <RouterLink to="create-class">
+              <LanguageWrapper
+                class="language-wrapper"
+                sign="M531x540S16d48470x494S1773e476x520S22b00515x486S15730471x460S21000492x498 M524x516S15a39477x485S15a31490x493S20600502x485"
+                portuguese="Criar Aula"
+              />
+            </RouterLink>
+          </div>
         </v-list-item>
         <v-list-item link @click="classesStore.deleteAllClasses">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            fill="currentColor"
-            class="bi bi-trash3"
-            viewBox="0 0 16 16"
-          >
-            <path
-              d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"
+          <div class="list-item">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-trash3"
+              viewBox="0 0 16 16"
+            >
+              <path
+                d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"
+              />
+            </svg>
+            <LanguageWrapper
+              class="language-wrapper"
+              sign="M536x525S1dc28465x481S15d40496x485S2dc06523x476 M523x523S21404485x518S15a58487x487S2c630503x506S15a56478x477"
+              portuguese="Local Storage"
             />
-          </svg>
-          &nbsp; localStorage
+          </div>
         </v-list-item>
       </v-list>
       <v-divider :thickness="2"></v-divider>
@@ -88,6 +108,16 @@ const drawer = ref<boolean>(false)
   display: flex;
   flex-direction: column;
   align-items: end;
+}
+
+.list-item {
+  display: flex;
+  align-items: center;
+  gap: 7px;
+}
+
+.language-wrapper {
+  display: inline-block;
 }
 
 .menu-btn {
