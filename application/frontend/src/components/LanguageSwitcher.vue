3bdeb6360c9a94fcb5fa360b96666da0e455e58c
wallyson ruan <wallysonruan@gmail.com>
2023-10-25 13:53:07 -0300

    refactor: simplified component by using vuetify native model/slots

--- application/frontend/src/components/LanguageSwitcher.vue
+++ application/frontend/src/components/LanguageSwitcher.vue
@@ -5,26 +5,23 @@ const languageStore = stores.languages()
 </script>
 <template>
   <div class="switcher">
-    <p>PT</p>
-    <v-switch class="switch" @click="languageStore.changeLanguage" hide-details> </v-switch>
-    <SignWriting
-      :display="true"
-      :width="40"
-      :height="20"
-      fsw="M542x516S14c0a459x493S14c10498x485S2480c526x488"
-    />
+    <v-switch v-model="languageStore.portuguese" @click="languageStore.changeLanguage" hide-details>
+      <template v-slot:prepend>
+        <p>PT</p>
+      </template>
+      <template v-slot:append>
+        <SignWriting
+          :display="true"
+          :width="40"
+          :height="20"
+          fsw="M542x516S14c0a459x493S14c10498x485S2480c526x488"
+        />
+      </template>
+    </v-switch>
   </div>
 </template>
 <style scoped lang="scss">
 .switcher {
-  display: flex;
-  align-items: center;
-  width: fit-content;
-  padding: 0.5rem;
-
-  .switch {
-    margin-left: 1rem;
-    margin-right: 1rem;
-  }
+  margin-right: 1.5rem;
 }
 </style>
