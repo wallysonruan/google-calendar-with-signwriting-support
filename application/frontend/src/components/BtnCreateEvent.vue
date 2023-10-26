3c78b2dda10692cf717d684f33dee4a29bb5daf7
wallyson ruan <wallysonruan@gmail.com>
2023-10-26 20:17:27 -0300

    refactor: extracted create btn

---
+++ application/frontend/src/components/BtnCreateEvent.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <v-btn class="create-btn" icon aria-label="Create Event">
+      <v-icon>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          class="bi bi-plus-lg"
+          viewBox="0 0 16 16"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z"
+          />
+        </svg>
+      </v-icon>
+    </v-btn>
+  </div>
+</template>
+<style scoped lang="scss">
+.create-btn {
+  position: fixed;
+  bottom: 40px;
+  left: 80%;
+  border-radius: 25%;
+}
+
+@media only screen and (min-width: 600px) {
+  .create-btn {
+    display: none;
+  }
+}
+</style>
--- application/frontend/src/components/NavigationDrawer.vue
+++ application/frontend/src/components/NavigationDrawer.vue
@@ -50,23 +50,6 @@ const navBar = stores.navbar()
       </div>
     </template>
   </v-navigation-drawer>
-  <v-btn class="menu-btn" icon aria-label="Create Event">
-    <v-icon>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="16"
-        height="16"
-        fill="currentColor"
-        class="bi bi-plus-lg"
-        viewBox="0 0 16 16"
-      >
-        <path
-          fill-rule="evenodd"
-          d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z"
-        />
-      </svg>
-    </v-icon>
-  </v-btn>
 </template>
 <style scoped lang="scss">
 .drawer {
@@ -84,16 +67,6 @@ const navBar = stores.navbar()
   display: inline-block;
 }
 
-.menu-btn {
-  position: fixed;
-  bottom: 40px;
-  left: 80%;
-  border-radius: 25%;
-}
-
 @media only screen and (min-width: 600px) {
-  .menu-btn {
-    display: none;
-  }
 }
 </style>
