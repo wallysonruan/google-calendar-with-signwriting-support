721a8a1454b65cdcf4710505192c6dccf3dcb398
wallyson ruan <wallysonruan@gmail.com>
2023-10-29 18:51:06 -0300

    wip: adding tests to SignWriting components again

--- application/frontend/src/components/SignWriting.vue
+++ application/frontend/src/components/SignWriting.vue
@@ -5,18 +5,50 @@ const props = defineProps<{
   fsw: string
   display: boolean
 }>()
+
+function containsSpacesOrCode(signsString: string): boolean {
+  if (signsString.includes(' ')) {
+    return true
+  }
+
+  if (signsString.includes('%20')) {
+    return true
+  }
+
+  return false
+}
+
+function getSignsSeparatedBySpacesOrCodes(signsString: string): string[] {
+  let emptyArray: string[] = []
+
+  if (typeof signsString === 'string') {
+    if (signsString.includes(' ')) {
+      return signsString.split(' ')
+    }
+
+    if (signsString.includes('%20')) {
+      return signsString.split('%20')
+    }
+  }
+
+  return emptyArray
+}
+
+const signs: string[] = containsSpacesOrCode(props.fsw)
+  ? getSignsSeparatedBySpacesOrCodes(props.fsw)
+  : [props.fsw]
 </script>
 
 <template>
-  <div v-show="display">
-    <div v-for="sign in props.fsw.split(' ')" class="signs">
+  <div v-show="display" class="signwriting-container">
+    <div v-for="sign in signs" class="sign">
       <fsw-sign :sign="sign" v-if="!isType(sign, 'punctuation')" />
       <fsw-symbol :symbol="sign" v-if="isType(sign, 'punctuation')" />
     </div>
   </div>
 </template>
 <style scoped lang="scss">
-.signs {
+.sign {
   display: flex;
   flex-direction: column;
   align-items: center;
--- application/frontend/src/components/__tests__/SignWriting.spec.ts
+++ application/frontend/src/components/__tests__/SignWriting.spec.ts
@@ -4,78 +4,71 @@ import { mount } from '@vue/test-utils'
 import SignWriting from '../SignWriting.vue'
 
 describe('SignWriting', () => {
-  it('Shoud accept only one sign and render it, instead of just taking two or more.', () => {
-    const wrapper = mount(SignWriting, {
-      fsw: {
-        sign: 'TEST',
-        display: false
-      }
-    })
-
-    expect(wrapper.find('img').exists()).toBe(true)
-  })
-
-  it('Should render no text', () => {
-    const wrapper = mount(SignWriting, {
-      fsw: {
-        sign: 'TEST',
-        display: true
-      }
-    })
-
-    expect(wrapper.text()).toBe('')
-  })
-
-  it('Shoud concatenate baseUrl with sign string', () => {
-    const wrapper = mount(SignWriting, {
-      fsw: {
-        sign: 'TEST',
-        display: true
-      }
-    })
-  })
-
-  it('Shoud NOT display if display is set to false', () => {
-    const wrapper = mount(SignWriting, {
-      props: {
-        fsw: 'TEST',
-        display: false
-      }
-    })
-
-    expect(wrapper.find('img').attributes('style')).toContain('display: none')
-    expect(wrapper.find('img').attributes('style')).not.toBe('')
-  })
-
-  it('Shoud accept multiple signs separated by SPACES, and iterate over them to generate a SignWriting for each', () => {
-    const wrapper = mount(SignWriting, {
-      props: {
-        fsw: 'TEST TEST TEST',
-        display: true
-      }
-    })
-
-    wrapper
-      .props('sign')
-      .split(' ')
-      .forEach((sign: string, index: number) => {
-        expect(wrapper.findAll('img')[index].attributes('src')).toContain(sign)
+  describe('Visibility', () => {
+    it('Should be visible when "display" is set to false', () => {
+      const wrapper = mount(SignWriting, {
+        props: {
+          fsw: '',
+          display: true
+        }
       })
-  })
 
-  it('Shoud accept multiple signs separated by "%20", and iterate over them to generate a SignWriting for each', () => {
-    const wrapper = mount(SignWriting, {
-      props: {
-        fsw: 'TEST%20TEST%20TEST',
-        display: true
-      }
+      expect(wrapper.findComponent('.signwriting-container').isVisible()).toBe(true)
     })
 
-    wrapper
-      .props('sign')
-      .split(' ')
-      .forEach((sign: string, index: number) => {
-        expect(wrapper.findAll('img')[index].attributes('src')).toContain(sign)
+    it('Should NOT be visible when "display" is set to false', () => {
+      const wrapper = mount(SignWriting, {
+        props: {
+          fsw: '',
+          display: false
+        }
       })
+
+      expect(wrapper.findComponent('.signwriting-container').isVisible()).toBe(false)
+    })
   })
+  describe('Quantity', () => {
+    it('If fsw string does not have spaces, should show only one sign', () => {
+      const wrapper = mount(SignWriting, {
+        props: {
+          fsw: 'M536x549S30c00482x483S14710521x504S28903490x521S22e04519x531',
+          display: true
+        }
+      })
+
+      expect(wrapper.find('.sign').exists()).toBe(true)
+    })
+  })
+
+  // it('Shoud accept multiple signs separated by SPACES, and iterate over them to generate a SignWriting for each', () => {
+  //   const wrapper = mount(SignWriting, {
+  //     props: {
+  //       fsw: 'TEST TEST TEST',
+  //       display: true
+  //     }
+  //   })
+
+  //   wrapper
+  //     .props('sign')
+  //     .split(' ')
+  //     .forEach((sign: string, index: number) => {
+  //       expect(wrapper.findAll('img')[index].attributes('src')).toContain(sign)
+  //     })
+  // })
+
+  // it('Shoud accept multiple signs separated by "%20", and iterate over them to generate a SignWriting for each', () => {
+  //   const wrapper = mount(SignWriting, {
+  //     props: {
+  //       fsw: 'TEST%20TEST%20TEST',
+  //       display: true
+  //     }
+  //   })
+
+  //   wrapper
+  //     .props('sign')
+  //     .split(' ')
+  //     .forEach((sign: string, index: number) => {
+  //       expect(wrapper.findAll('img')[index].attributes('src')).toContain(sign)
+  //     })
+  // })
 })
