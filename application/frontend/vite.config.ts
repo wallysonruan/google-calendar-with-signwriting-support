443b78f95f1e85014bee2bee2f3e40d3304541ee
wallyson ruan <wallysonruan@gmail.com>
2023-10-30 13:32:04 -0300

    refactor: removed unecessary commented out code

--- application/frontend/vite.config.ts
+++ application/frontend/vite.config.ts
@@ -1,9 +1,7 @@
 import { fileURLToPath, URL } from 'node:url'
-// import { nodeResolve } from '@rollup/plugin-node-resolve'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-// import vueJsx from '@vitejs/plugin-vue-jsx'
 import vuetify from 'vite-plugin-vuetify'
 
 // https://vitejs.dev/config/
@@ -19,19 +17,15 @@ export default defineConfig({
     vuetify({
       // https://stackoverflow.com/questions/72350551/combination-of-vue-3-vuetify-3-vue-test-utils-results-in-could-not-find-in
       autoImport: true
-    }),
-    // nodeResolve()
+    })
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  // base: process.env.REPOSITORY_NAME, // This will prevent the resources disappearing when they change from one GitHub repository to another
   base: '/google-calendar-with-signwriting-support/',
   build: {
-    // emptyOutDir: true, // force destroy of bundled assets when outside of project dir
-    // outDir: '../../', // delete everything, only the output will remain
     sourcemap: true,
     rollupOptions: {
       output: {
